2e8e1e03868053fcd6c8f3d2f2dd04d22b7dd83c
faresahmed1153 <faresahmed3090@gmail.com>
2022-05-05 19:07:36 +0200

    add put method

--- modules/note/note.router.js
+++ modules/note/note.router.js
@@ -1,11 +1,12 @@
 const { auth } = require("../../middleware/auth");
 const { noteValidation } = require("../../validation/validation");
 const { addNote, updateNote, deleteNote, notes } = require("./controller/note");
+
 const router = require("express").Router();
 
 router.get("/note", auth(), notes);
 router.post("/note", auth(), noteValidation(), addNote);
-router.patch("/note", auth(), noteValidation(), updateNote);
+router.put("/note", auth(), noteValidation(), updateNote);
 router.delete("/note", auth(), deleteNote);
 
 module.exports = router;
