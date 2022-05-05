f30b3437264ba8f45a488366b67f9c2f2c9dbf42
faresahmed1153 <faresahmed3090@gmail.com>
2022-05-05 16:23:50 +0200

    add cors

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.use(function (req, res, next) {
   );
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,PATCH");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization, access-control-allow-origin"
+  );
   next();
 });
 app.use(express.json());
