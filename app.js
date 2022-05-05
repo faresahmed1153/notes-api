b05389a5b1e5f4a9ffe85e7261119e80bc86b26e
faresahmed1153 <faresahmed3090@gmail.com>
2022-05-05 16:39:36 +0200

    add cors

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const connectDB = require("./DB/connection");
 const { userRouter, noteRouter } = require("./modules/index.router");
 const app = express();
-app.use(function (req, res, next) {
+/* app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
@@ -16,14 +16,13 @@ app.use(function (req, res, next) {
     "http://localhost:3000"
   );
   next();
-});
+}); */
 const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
 require("dotenv").config();
 app.use(userRouter, noteRouter);
-app.use(express.json());
 connectDB();
 app.listen(process.env.PORT || 5000, () => {
   console.log("running......");
