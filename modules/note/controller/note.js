fdd80189ff5febae27874112bbcbc0a64ebc0655
faresahmed1153 <faresahmed3090@gmail.com>
2022-05-05 04:02:10 +0200

    replace bcrypt

--- modules/note/controller/note.js
+++ modules/note/controller/note.js
@@ -1,49 +1,46 @@
 const noteModel = require("../../../DB/model/Note");
 
 const addNote = async (req, res) => {
-  const { title, desc} = req.body;
-  
-    try {
-      const newNote = new noteModel({
-        title,
-        desc,
-        createdBy: req.user._id,
-      });
-      const savedNote = await newNote.save();
+  const { title, desc } = req.body;
 
-      res.json({ message: "Done", savedNote });
-    } catch (error) {
-      res.json({ err: error.name });
-    }
-  
+  try {
+    const newNote = new noteModel({
+      title,
+      desc,
+      createdBy: req.user._id,
+    });
+    const savedNote = await newNote.save();
+
+    res.json({ message: "Done", savedNote });
+  } catch (error) {
+    res.json({ err: error.name });
+  }
 };
 
 const updateNote = async (req, res) => {
-  const { title, desc } = req.body;
- 
-    try {
-      const updatedNote = await noteModel.findOneAndUpdate(
-        { createdBy: req.user._id },
-        { title, desc },
-        { new: true }
-      );
+  const { title, desc, noteId } = req.body;
 
-      if (updatedNote) {
-        res.json({ message: "Done", updatedNote });
-      } else {
-        res.json({ message: "in-valid id", updatedNote });
-      }
-    } catch (error) {
-      res.json({ message: "catch err", error });
+  try {
+    const updatedNote = await noteModel.findOneAndUpdate(
+      { _id: noteId },
+      { title, desc },
+      { new: true }
+    );
+
+    if (updatedNote) {
+      res.json({ message: "Done", updatedNote });
+    } else {
+      res.json({ message: "in-valid id", updatedNote });
     }
-  
+  } catch (error) {
+    res.json({ message: "catch err", error });
+  }
 };
 
 const deleteNote = async (req, res) => {
+  const { noteId } = req.body;
   try {
-    const note = await noteModel
-      .deleteOne({ createdBy: req.user._id })
-      .select("-password");
+    const note = await noteModel.deleteOne({ _id: noteId }).select("-password");
 
     if (note.deletedCount) {
       res.json({ message: "Done", note });
@@ -56,19 +53,17 @@ const deleteNote = async (req, res) => {
 };
 
 const notes = async (req, res) => {
-  
   try {
     const note = await noteModel
-      
-      .find({createdBy:req.user._id})
-      .populate({ path: "createdBy", select: "-password"});
+
+      .find({ createdBy: req.user._id })
+      .populate({ path: "createdBy", select: "-password" });
 
     res.json({ message: "Done", note });
   } catch (error) {
     res.json({ messaage: "catch error", error });
   }
 };
-
 
 module.exports = {
   addNote,
