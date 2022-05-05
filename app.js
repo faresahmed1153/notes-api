084a98267a7f39a6b66fb326d817e9bbc5defde1
faresahmed1153 <faresahmed3090@gmail.com>
2022-05-05 14:35:44 +0200

    add cors

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ const express = require("express");
 const connectDB = require("./DB/connection");
 const { userRouter, noteRouter } = require("./modules/index.router");
 const app = express();
-
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000/"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(express.json());
 require("dotenv").config();
 app.use(userRouter, noteRouter);
