15616869c70db2c66e0bae930bbf19ef5098cc7c
faresahmed1153 <faresahmed3090@gmail.com>
2022-05-05 06:40:16 +0200

    add cors

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const { userRouter, noteRouter } = require("./modules/index.router");
 const app = express();
 const cors = require(cors);
 app.use(cors({ origin: "*" }));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 app.use(express.json());
 require("dotenv").config();
 app.use(userRouter, noteRouter);
