a1b91a9184a057886ac8671502e5720dda9f56ce
faresahmed1153 <faresahmed3090@gmail.com>
2022-05-05 04:12:48 +0200

    replace bcrypt

--- modules/note/note.router.js
+++ modules/note/note.router.js
@@ -1,11 +1,11 @@
 const { auth } = require("../../middleware/auth");
 const { noteValidation } = require("../../validation/validation");
-const { addNote, updateNote, deleteNote, notes} = require("./controller/note");
+const { addNote, updateNote, deleteNote, notes } = require("./controller/note");
 const router = require("express").Router();
 
 router.get("/note", auth(), notes);
-router.post("/note", auth(),noteValidation(), addNote);
+router.post("/note", auth(), noteValidation(), addNote);
 router.patch("/note", auth(), noteValidation(), updateNote);
-router.delete("/product", auth(), deleteNote);
+router.delete("/note", auth(), deleteNote);
 
 module.exports = router;
